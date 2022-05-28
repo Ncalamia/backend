5118dff2afb822531b7471a854efe747035cf4ed
Nicole Calamia <nikki@Nicoles-MacBook-Pro-4.local>
2022-05-28 17:45:24 -0400

    added seed data and it is working on localhost

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const seedMSA = require('./models/msadata.js')
 require('dotenv').config()
 
 // CONNECTIONS
-// mongoose.connect('mongodb://localhost:27017/app')
-// mongoose.connection.once('open', () => {
-//     console.log('connected to mongod...')
-// })
+mongoose.connect('mongodb://localhost:27017/app')
+mongoose.connection.once('open', () => {
+    console.log('connected to mongod...')
+})
 
 //Port
 //___________________
@@ -28,9 +28,9 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI , () => {
-	console.log('connected to mongo')
-})
+// mongoose.connect(MONGODB_URI , () => {
+// 	console.log('connected to mongo')
+// })
 
 
 // MIDDLEWARE
