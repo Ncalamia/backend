65be48663ba6183a4e22b2ef0a2b0d017dcbcdb2
Maha Mamoor <mamoorm@gmail.com>
2022-05-29 13:59:18 -0400

    forum schema and data added

---
+++ models/forumdata.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    username: "Meowing Maha",
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCWNt9mAyyhSSjuFvPgqC35txwRAH_n641_Q&usqp=CAU.jpg",
+    comment: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    emoji: "🥺🥺"
+  },
+  {
+    username: "Gramma Nikki",
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfW8v32GAWm8jswZarCCpfd3vSMoxhg4gDoQ&usqp=CAU.jpg",
+    comment: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    emoji: "😔😔"
+  }
+]
---
+++ models/forumschema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Forumschema = new mongoose.Schema({
+  username: String,
+  avatar: String,
+  comment: {type: String, required: true},
+  emoji: String
+})
+
+const forumcollection = mongoose.model("forumcollection", Forumschema)
+module.exports = forumcollection
